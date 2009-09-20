09993c546cccfdd4ca9e660e046b5c1b5b983a66
ZhangJinzhu <wosmvp@gmail.com>
2009-09-20 22:00:01 +0800

    test dataType xml

--- server.rb
+++ server.rb
@@ -17,3 +17,14 @@ end
 get '/json' do
   JSON.generate({:name => 'jinzhu',:sex => 'm',:email => 'wosmvp@gmail.com'})
 end
+
+get '/xml' do
+  content_type 'text/xml', :charset => 'utf-8'
+  builder do |xml|
+    xml.instruct!(:xml, :version => '1.1', :encoding => 'UTF-8')
+    xml.person do
+      xml.name "Jinzhu"
+      xml.email 'wosmvp@gmail.com'
+    end
+  end
+end
--- testcases/dataType.js
+++ testcases/dataType.js
@@ -20,3 +20,25 @@ test("jQuery.cacheAjax() - dataType json", function() {
 
   start();
 });
+
+// doesn't work with firefox-3.5/linux? works very well with opera/linux,chromium/linux
+test("jQuery.cacheAjax() - dataType xml", function() {
+  expect(4); // beforeSend,2 success,complete
+  stop();
+
+  successCacheAjax({
+    url: '/xml',
+    key: 'xml-cache',
+    dataType: 'xml'
+  });
+
+  successCacheAjax({
+    url: '/xml',
+    key: 'xml-cache',
+    dataType: 'xml',
+    success: function(e){
+        equals(1,$(e).find('name').size(),'xml element number')
+      }
+    });
+  start();
+});
